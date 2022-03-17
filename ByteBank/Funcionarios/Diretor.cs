cdd3085ea17dbc9a2052f31801a6a272b3ae5bc9
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-17 09:09:32 -0300

    Aula 08.Mão na Massa - 01.Conhecendo o Problema do Cliente - Alura CSharp Parte 3

---
+++ ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor
+    {
+        public string Nome;
+        public string CPF;
+        public Double Salario;
+
+        public Double GetBonificacao()
+        {
+            return Salario;
+        }
+    }
+}
---
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public String Nome;
+        public String CPF;
+        public Double Salario;
+        
+        public Double GetBonificacao()
+        {
+        return Salario * 0.10;
+        }
+    }    
+}
